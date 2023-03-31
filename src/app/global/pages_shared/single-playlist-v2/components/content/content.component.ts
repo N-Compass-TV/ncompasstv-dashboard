854d0f33be97c02089a5b428c21846ff79d0f73b
earlvhin <earlgabuat@gmail.com>
2023-03-31 20:18:02 +0800

    thumbnails

--- src/app/global/pages_shared/single-playlist-v2/components/content/content.component.ts
+++ src/app/global/pages_shared/single-playlist-v2/components/content/content.component.ts
@@ -1,11 +1,14 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { API_CONTENT } from 'src/app/global/models';
+import { IsimagePipe } from 'src/app/global/pipes';
+import { environment } from 'src/environments/environment';
 import { PlaylistContentControls } from '../../constants/PlaylistContentControls';
 
 @Component({
 	selector: 'app-content',
 	templateUrl: './content.component.html',
-	styleUrls: ['./content.component.scss']
+	styleUrls: ['./content.component.scss'],
+	providers: [IsimagePipe]
 })
 export class ContentComponent implements OnInit {
 	@Input() content: API_CONTENT;
@@ -13,9 +16,25 @@ export class ContentComponent implements OnInit {
 	@Output() control_click: EventEmitter<any> = new EventEmitter();
 	@Output() content_selected: EventEmitter<string> = new EventEmitter();
 	contentName: string;
+	fs_screenshot = `${environment.third_party.filestack_screenshot}`;
 	playlistContentControls = PlaylistContentControls;
 
-	constructor() {}
+	constructor(private _isImage: IsimagePipe) {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		this.prepareThumbnails();
+	}
+
+	prepareThumbnails() {
+		/** webm assets  */
+		if (this.content.fileType === 'webm')
+			this.content.thumbnail = `${this.content.url}${this.content.fileName.substr(0, this.content.fileName.lastIndexOf('.') + 1)}jpg`;
+
+		/** mp4 assets */
+		if (this.content.fileType === 'mp4' && this.content.handlerId)
+			this.content.thumbnail = `${this.content.url}${this.content.fileName.substr(0, this.content.fileName.lastIndexOf('.') + 1)}jpg`;
+
+		/** image assets */
+		if (this._isImage.transform(this.content.fileType)) this.content.thumbnail = `${this.content.url}${this.content.fileName}`;
+	}
 }
