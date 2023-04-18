5c9fcc3c2c0fbd0d367277caf01906dea0469883
earlvhin <earlgabuat@gmail.com>
2023-04-18 10:36:39 +0800

    Single Playlist V2

--- src/app/global/pages_shared/single-playlist-v2/components/content-settings/content-settings.component.ts
+++ src/app/global/pages_shared/single-playlist-v2/components/content-settings/content-settings.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
-  selector: 'app-content-settings',
-  templateUrl: './content-settings.component.html',
-  styleUrls: ['./content-settings.component.scss']
+	selector: 'app-content-settings',
+	templateUrl: './content-settings.component.html',
+	styleUrls: ['./content-settings.component.scss']
 })
 export class ContentSettingsComponent implements OnInit {
+	constructor(@Inject(MAT_DIALOG_DATA) public content: any) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
