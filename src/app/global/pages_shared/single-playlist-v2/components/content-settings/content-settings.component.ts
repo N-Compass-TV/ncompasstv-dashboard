1a5c825b408a952786efd944048da301ee4afae2
earlvhin <earlgabuat@gmail.com>
2023-05-05 14:37:57 +0800

    Prepping blacklist

--- src/app/global/pages_shared/single-playlist-v2/components/content-settings/content-settings.component.ts
+++ src/app/global/pages_shared/single-playlist-v2/components/content-settings/content-settings.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material';
+import { API_CONTENT } from '../../../../models';
+import { PlaylistService } from '../../../../services';
 
 @Component({
 	selector: 'app-content-settings',
@@ -12,7 +14,17 @@ export class ContentSettingsComponent implements OnInit {
 		duration: ['']
 	});
 
-	constructor(private _formBuilder: FormBuilder, @Inject(MAT_DIALOG_DATA) public content: any) {}
+	constructor(private _formBuilder: FormBuilder, @Inject(MAT_DIALOG_DATA) public content: API_CONTENT, private _playlist: PlaylistService) {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		this.getBlacklistData();
+	}
+
+	getBlacklistData() {
+		this._playlist.get_blacklisted_by_id(this.content.playlistContentId).subscribe({
+			next: (res) => {
+				console.log(res);
+			}
+		});
+	}
 }
