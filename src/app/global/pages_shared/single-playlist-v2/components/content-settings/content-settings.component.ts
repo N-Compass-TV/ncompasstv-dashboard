d47082861ea5ae5cde4b8334d5d15feb1b6b50d9
earlvhin <earlgabuat@gmail.com>
2023-05-06 22:30:00 +0800

    Display host licenses for play where

--- src/app/global/pages_shared/single-playlist-v2/components/content-settings/content-settings.component.ts
+++ src/app/global/pages_shared/single-playlist-v2/components/content-settings/content-settings.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material';
-import { API_CONTENT } from '../../../../models';
+import { API_CONTENT, API_HOST, API_LICENSE_PROPS } from '../../../../models';
 import { PlaylistService } from '../../../../services';
 
 @Component({
@@ -14,14 +14,19 @@ export class ContentSettingsComponent implements OnInit {
 		duration: ['']
 	});
 
-	constructor(private _formBuilder: FormBuilder, @Inject(MAT_DIALOG_DATA) public content: API_CONTENT, private _playlist: PlaylistService) {}
+	constructor(
+		private _formBuilder: FormBuilder,
+		@Inject(MAT_DIALOG_DATA)
+		public contentData: { playlistContent: API_CONTENT; hostLicenses: { host: API_HOST; licenses: API_LICENSE_PROPS[] } },
+		private _playlist: PlaylistService
+	) {}
 
 	ngOnInit() {
 		this.getBlacklistData();
 	}
 
 	getBlacklistData() {
-		this._playlist.get_blacklisted_by_id(this.content.playlistContentId).subscribe({
+		this._playlist.get_blacklisted_by_id(this.contentData.playlistContent.playlistContentId).subscribe({
 			next: (res) => {
 				console.log(res);
 			}
