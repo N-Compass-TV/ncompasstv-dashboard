f2fcce424029e6a69790cea95da96dac8d2c023a
earlvhin <earlgabuat@gmail.com>
2023-05-19 16:12:24 +0800

    Playlist Draggables

--- src/app/global/pages_shared/single-playlist-v2/services/single-playlist.service.ts
+++ src/app/global/pages_shared/single-playlist-v2/services/single-playlist.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from 'src/app/global/services/base.service';
+import { NewPlaylistContent } from '../interface/NewPlaylistContent';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class SinglePlaylistService extends BaseService {
+	addContent(data: NewPlaylistContent) {
+		return this.postRequest('playlistsv2/addcontent', data);
+	}
+
+	createWhitelistRecord() {}
+
 	getPlaylistData(playlistId: string) {
 		return this.getRequest(`playlistsv2?playlistid=${playlistId}`);
 	}
 
 	getPlaylistHosts(playlistId: string) {
 		return this.getRequest(`playlistsv2/gethostlicenses?playlistid=${playlistId}`).map((data: any) => data.hostLicenses);
+	}
+
+	getWhitelistData(playlistContentId: string) {
+		return this.getRequest(`playlistsv2/GetLicensePlaylistContents?playlistContentId=${playlistContentId}`);
 	}
 }
