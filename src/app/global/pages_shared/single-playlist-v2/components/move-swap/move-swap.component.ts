f4bc680248ce1ed1dcc20b3494fa825fde6ca730
earlvhin <earlgabuat@gmail.com>
2023-07-05 19:26:45 +0800

    move and swap ui

--- src/app/global/pages_shared/single-playlist-v2/components/move-swap/move-swap.component.ts
+++ src/app/global/pages_shared/single-playlist-v2/components/move-swap/move-swap.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { MOVE_SWAP } from '../../constants/MoveSwap';
+import { ButtonGroup } from '../../type/ButtonGroup';
+import { MAT_DIALOG_DATA } from '@angular/material';
+import { API_CONTENT } from 'src/app/global/models';
 
 @Component({
 	selector: 'app-move-swap',
@@ -10,16 +13,37 @@ import { MOVE_SWAP } from '../../constants/MoveSwap';
 export class MoveSwapComponent implements OnInit {
 	moveSwap = MOVE_SWAP;
 	moveSwapForm: FormGroup;
+	selectedAction: ButtonGroup = this.moveSwap[0];
+	playlistContent;
+	playlistCount = 0;
 
-	constructor(private _formBuilder: FormBuilder) {}
+	constructor(
+		private _formBuilder: FormBuilder,
+		@Inject(MAT_DIALOG_DATA)
+		public data: {
+			playlistContent: API_CONTENT;
+			playlistContentCount: number;
+		}
+	) {
+		this.playlistContent = this.data.playlistContent;
+		this.playlistCount = this.data.playlistContentCount;
+	}
 
 	ngOnInit() {
 		this.moveSwapForm = this._formBuilder.group({
-			seq: [
-				{
-					value: ''
-				}
-			]
+			seq: [this.playlistContent.seq, [Validators.required, this.numberRangeValidator(this.playlistCount)]]
 		});
+	}
+
+	numberRangeValidator(maxValue: number): ValidatorFn {
+		return (control: AbstractControl): { [key: string]: any } | null => {
+			const value = Number(control.value);
+
+			if (isNaN(value) || value < 1 || value > maxValue) {
+				return { numberRange: true };
+			}
+
+			return null;
+		};
 	}
 }
