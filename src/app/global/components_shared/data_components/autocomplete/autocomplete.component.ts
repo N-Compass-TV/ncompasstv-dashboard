aa154d93abd82c5f9330f1974aa6272b703b6455
RenellaMarie <casinillorenellamarie@gmail.com>
2023-12-15 13:11:35 +0800

    feature(nsb-4172): recommit changes

--- src/app/global/components_shared/data_components/autocomplete/autocomplete.component.ts
+++ src/app/global/components_shared/data_components/autocomplete/autocomplete.component.ts
@@ -2,43 +2,61 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
-import { UI_AUTOCOMPLETE } from 'src/app/global/models';
+import { UI_AUTOCOMPLETE, UI_AUTOCOMPLETE_DATA } from 'src/app/global/models';
 
 @Component({
-    selector: 'app-autocomplete',
-    templateUrl: './autocomplete.component.html',
-    styleUrls: ['./autocomplete.component.scss']
+	selector: 'app-autocomplete',
+	templateUrl: './autocomplete.component.html',
+	styleUrls: ['./autocomplete.component.scss']
 })
 export class AutocompleteComponent implements OnInit {
-    @Input() field_data: UI_AUTOCOMPLETE = {
-        label: 'Label',
-        placeholder: 'Type anything',
-        data: []
-    };
-    @Output() value_selected: EventEmitter<{ id: string; value: string }> = new EventEmitter();
+	@Input() field_data: UI_AUTOCOMPLETE = {
+		label: 'Label',
+		placeholder: 'Type anything',
+		data: [],
+		initial_value: []
+	};
+	@Input() initial_value: UI_AUTOCOMPLETE_DATA[];
+	@Output() value_selected: EventEmitter<{ id: string; value: string }> = new EventEmitter();
 
-    autoCompleteControl = new FormControl();
-    filteredOptions!: Observable<any[]>;
+	autoCompleteControl = new FormControl();
+	filteredOptions!: Observable<any[]>;
 
-    constructor() {}
+	constructor() {}
 
-    ngOnInit() {
-        this.filteredOptions = this.autoCompleteControl.valueChanges.pipe(
-            startWith(''),
-            map((keyword) => this._filter(keyword))
-        );
-    }
+	ngOnInit() {
+		this.filteredOptions = this.autoCompleteControl.valueChanges.pipe(
+			startWith(''),
+			map((keyword) => this._filter(keyword))
+		);
+	}
 
-    displayOption(option: any): string {
-        return option ? option.value : '';
-    }
+	ngAfterViewInit() {
+		if (this.field_data.initial_value != null) {
+			this.autoCompleteControl.setValue(this.field_data.initial_value[0]);
+		}
+	}
 
-    private _filter(keyword: any) {
-        const filterValue = keyword.hasOwnProperty('value') ? keyword.value.toLowerCase() : keyword.toLowerCase();
-        return this.field_data.data.filter((option) => option.value.toLowerCase().includes(filterValue));
-    }
+	// For autocomplete to change value without reloading the page upon saved
+	ngOnChanges() {
+		this.field_data = this.field_data;
 
-    valueSelected(e) {
-        this.value_selected.emit(e.option.value.id);
-    }
+		console.log('FIELD', this.field_data);
+		// if (this.field_data.initial_value != null) {
+		// 	this.field_data.initial_value[0] = this.field_data.initial_value[0];
+		// }
+	}
+
+	displayOption(option: any): string {
+		return option ? option.value : '';
+	}
+
+	private _filter(keyword: any) {
+		const filterValue = keyword.hasOwnProperty('value') ? keyword.value.toLowerCase() : keyword.toLowerCase();
+		return this.field_data.data.filter((option) => option.value.toLowerCase().includes(filterValue));
+	}
+
+	valueSelected(e) {
+		this.value_selected.emit(e.option.value.id);
+	}
 }
--- src/app/global/models/ui_autocomplete.model.ts
+++ src/app/global/models/ui_autocomplete.model.ts
@@ -1,11 +1,12 @@
 export interface UI_AUTOCOMPLETE {
-    label: string;
-    placeholder: string;
-    data: { id: string; value: any }[];
-    disabled?: boolean;
+	label: string;
+	placeholder: string;
+	data: { id: string; value: any }[];
+	disabled?: boolean;
+	initial_value?: { id: string; value: any }[];
 }
 
 export interface UI_AUTOCOMPLETE_DATA {
-    id: string,
-    value: any
+	id: string;
+	value: any;
 }
