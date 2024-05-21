49a146a790560ddc56c7ca02caa4d28fc61c92e0
earlvhin <earlgabuat@gmail.com>
2024-05-21 12:07:48 +0800

    chore(nctv-253): change to snakecase

--- src/app/global/components_shared/data_components/dealer-autocomplete/dealer-autocomplete.component.ts
+++ src/app/global/components_shared/data_components/dealer-autocomplete/dealer-autocomplete.component.ts
@@ -20,8 +20,8 @@ export class DealerAutocompleteComponent implements OnInit {
     @Input() active_only: boolean = false;
     @Input() isDealerAdmin = false;
     @Input() isDisabled = false;
-    @Input() fieldLabel = 'Select Dealer Business Name';
-    @Input() fieldPlaceholder = 'Ex. NCompass TV';
+    @Input() field_label = 'Select Dealer Business Name';
+    @Input() field_placeholder = 'Ex. NCompass TV';
     @Output() dealer_selected: EventEmitter<any> = new EventEmitter();
     @Output() no_data_found = new EventEmitter();
 
@@ -48,8 +48,8 @@ export class DealerAutocompleteComponent implements OnInit {
 
     setAutocomplete() {
         this.dealers_data = {
-            label: this.fieldLabel,
-            placeholder: this.fieldPlaceholder,
+            label: this.field_label,
+            placeholder: this.field_placeholder,
             data: this.dealers,
             initialValue: this.initial_value || [],
             unselect: true,
